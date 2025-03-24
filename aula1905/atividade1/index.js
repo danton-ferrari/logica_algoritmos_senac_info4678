aa82ec970cb106cc7ec713ae52fde74e5ac89c01
DANTONFERRARI <dgabrielferrari7@gmail.com>
2025-03-24 19:44:49 -0300

    Add atividades da aula

---
+++ aula1905/atividade1/index.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+    var nome = "Relampago Marquinhos";
+    var idade = 28;
+    var peso = 83.5;
+    var isSolteiro = true;
+    var exibirInformacoes = nome + " " + idade + " " + peso + " " + isSolteiro;
+    alert(exibirInformacoes);
+}
+
+const b1azul = document.querySelector(".b1")
+
+// function changeColor() {
+//     b1azul.style.background = "blue";
+// }
