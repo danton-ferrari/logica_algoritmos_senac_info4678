a3a9c8da032f8d194ae7612bae15c6d3fa2f837e
DANTONFERRARI <dgabrielferrari7@gmail.com>
2025-03-26 20:48:41 -0300

    Add exercicio 6  pronto da aula 26/03

---
+++ aula2603/exercicio6/index.js
@@ -0,0 +1,4 @@
+function antesDepois() {
+    var numero = Number(document.getElementById("numero").value);
+    alert(`O antecessor de ${numero} é ${numero-1}. O sucessor de ${numero} é ${numero+1}`)
+}
