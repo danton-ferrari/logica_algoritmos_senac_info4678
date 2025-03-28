a14cc2b282c5266068493b4c4157d9e928a7c612
DANTONFERRARI <dgabrielferrari7@gmail.com>
2025-03-28 20:32:17 -0300

    Editando o valor do salario do exercicio 15 da aula 28/03/2025

--- aula2603/exercicio15/index.js
+++ aula2603/exercicio15/index.js
@@ -1,4 +1,4 @@
 function calcularSalario(id) {
     var diasTrabalhados = Number(document.getElementById(id).value);
-    alert(`Você receberá R$${(diasTrabalhados*8).toFixed(2)} pelos ${diasTrabalhados} dias trabalhados.`);
+    alert(`Você receberá R$${(diasTrabalhados*25).toFixed(2)} pelos ${diasTrabalhados} dias trabalhados.`);
 }
